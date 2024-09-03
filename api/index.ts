31eeaafb535aeb81c364be6f7790977eb68cf887
rizahoemae <humaida.mawaddatur@gmail.com>
2024-09-03 15:02:24 +0700

    fix init setup vercel

--- api/index.ts
+++ api/index.ts
@@ -8,17 +8,17 @@ import bodyParser from "body-parser";
 import { typeDefs, resolvers } from "../schema";
 
 const app = express();
-const httpServer = http.createServer(app);
 
+const httpServer = http.createServer(app);
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+app.use(cors(), express.json(), expressMiddleware(server));
 
 await server.start();
 
-app.use(cors(), express.json(), expressMiddleware(server));
 
 await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:4000`);
